f3f2b65cf5615e806814f6a92e3f8103d88f04e2
lucask42 <lucasbkellner@gmail.com>
2017-02-10 08:54:17 -0800

    Add files via upload

---
+++ DNA.js
@@ -0,0 +1,30 @@
+function DNA(genes){
+  if (genes){
+    this.genes = genes;
+  } else{
+    this.genes = [];
+    this.genes[0] = p5.Vector.random2D();
+    for (var i = 1; i < lifespan; i ++){
+      this.genes[i] = this.genes[i-1].rotate(random(1));
+      this.genes[i].setMag(0.02);
+    }
+  }
+
+  this.crossover = function(partner){
+    var newdna = [];
+    var mid = floor(random(this.genes.length));
+    var mutationsite = floor(random(this.genes.length));
+    for (var i = 0; i < this.genes.length; i++){
+      if (i > mid){
+        newdna[i] = this.genes[i];
+      } else{
+        newdna[i] = partner.genes[i];
+      }
+    }
+    //newdna[mutationsite] = p5.Vector.random2D();
+    //newdna[mutationsite].setMag(0.02)
+    newdna[mutationsite].rotate(random(1));
+
+    return new DNA(newdna);
+  }
+}
---
+++ rockets.js
@@ -0,0 +1,53 @@
+function Rocket(dna){
+  this.pos = createVector(width/2, height);
+  this.vel = createVector();
+  this.acc = createVector();
+  this.completed = false;
+  if (dna) {
+    this.dna = dna;
+  } else {
+  this.dna = new DNA();
+  }
+  this.fitness = 0;
+
+  this.applyForce = function(force){
+    this.acc.add(force);
+  }
+
+  this.calcFitness = function(){
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+    this.fitness = 1 / d; //map(d, 0, width, width, 0);
+    if (this.completed){
+      //this.fitness *= 10;
+    }
+
+  }
+
+  this.update = function(){
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    if (d < 10){
+      this.completed = true
+      this.pos = target.copy();
+    }
+    this.applyForce(this.dna.genes[count]);
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+
+
+  }
+
+  this.show = function(){
+    push();
+    noStroke();
+    fill(255,100);
+    translate(this.pos.x, this.pos.y)
+    rotate(this.vel.heading());
+    rectMode(CENTER);
+    rect(0,0, 5, 2);
+    pop();
+
+  }
+}
